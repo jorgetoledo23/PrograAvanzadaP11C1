543a1e86c9b9f90648f0ca57050be73eb4a84617
Jorge <jorge.toledo.6@hotmail.com>
2022-03-23 13:54:47 -0300

    Clase 23 Marzo 2022

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un Numero de MAX 3 Cifras: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"Centenas: {num / 100}");
+            Console.WriteLine($"Decenas: {(num % 100) / 10}");
+            Console.WriteLine($"Unidades: {(num % 10)}");
+        }
+    }
+}
