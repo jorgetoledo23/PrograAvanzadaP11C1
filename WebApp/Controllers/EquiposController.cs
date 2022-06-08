ecae88caacc595b9a043ccc9109c888c9e58e4d7
Jorge <jorge.toledo.6@hotmail.com>
2022-06-08 13:45:52 -0400

    Equipos

--- WebApp/Controllers/EquiposController.cs
+++ WebApp/Controllers/EquiposController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -8,13 +9,20 @@ namespace WebApp.Controllers
     {
 
         private readonly AppDbContext _context;
-        public EquiposController(AppDbContext context)
+        private readonly IWebHostEnvironment _environment;
+        public EquiposController(AppDbContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
         public IActionResult Index()
         {
-            var Equipos = _context.tblEquipos.ToList();
+            var Equipos = _context.tblEquipos
+                .Include(e => e.Marca)
+                .Include(e => e.Linea)
+                .Include(e => e.Categoria)
+                .ToList();
+
             return View(Equipos);
         }
 
@@ -27,9 +35,33 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult CrearEquipo(Equipo E)
+        public async Task<IActionResult> CrearEquipo(Equipo E)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (E.ImagenFile == null) E.ImagenUrl = "no-disponible.png";
+                else
+                {
+                    string wwwRootPath = _environment.WebRootPath;
+                    string fileName = Path.GetFileNameWithoutExtension(E.ImagenFile.FileName);
+                    string extension = Path.GetExtension(E.ImagenFile.FileName);
+                    E.ImagenUrl = fileName + DateTime.Now.ToString("ddMMyyyyHHmmss") + extension; //foto08062022131545.png 
+
+                    string path = Path.Combine(wwwRootPath + "/img/" + E.ImagenUrl);
+
+                    using (var fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        await E.ImagenFile.CopyToAsync(fileStream);
+                    }
+                }
+                _context.Add(E);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(E);
+            }
         }
 
 
