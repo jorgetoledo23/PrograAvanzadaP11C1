f594d269cb573c40d3c9f954995457f66a5c0693
Jorge <jorge.toledo.6@hotmail.com>
2022-06-08 13:11:28 -0400

    Equipos

---
+++ WebApp/Controllers/EquiposController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class EquiposController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public EquiposController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Equipos = _context.tblEquipos.ToList();
+            return View(Equipos);
+        }
+
+        public IActionResult CrearEquipo()
+        {
+            ViewData["Marcas"] = new SelectList(_context.tblMarcas.ToList(), "Id", "Name");
+            ViewData["Lineas"] = new SelectList(_context.tblLineas.ToList(), "Id", "Name");
+            ViewData["Categorias"] = new SelectList(_context.tblCategorias.ToList(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearEquipo(Equipo E)
+        {
+            return View();
+        }
+
+
+    }
+}
--- WebApp/Models/Equipo.cs
+++ WebApp/Models/Equipo.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
 {
     public class Equipo
     {
         public int Id { get; set; }
+
+        [DisplayName("Nombre")]
         public string? Descripcion { get; set; }
+        
+        [DisplayName("Descripcion Larga")]
         public string? DescripcionLarga { get; set; }
         public string? Ram { get; set; }
         public string? Procesador { get; set; }
@@ -14,7 +19,10 @@ namespace WebApp.Models
         public bool GPUIntegrada { get; set; }
         public bool GPUDedicada { get; set; }
         public string? GPUDedicadaName { get; set; }
+       
         public string? HzPantalla { get; set; }
+        
+        [DisplayName("Pulgadas Pantalla")]
         public string? InPantalla { get; set; }
         public string? ImagenUrl { get; set; }
 
@@ -30,9 +38,6 @@ namespace WebApp.Models
 
         public int CategoriaId { get; set; }
         public Categoria? Categoria { get; set; }
-
-
-
 
     }
 }
